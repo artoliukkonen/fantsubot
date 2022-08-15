ae69013e9870a2bd78c55c6a20e244ff7db0c07f
Arto Liukkonen <arto@51.fi>
2022-08-15 10:26:20 +0300

    fix getWeek

--- app.js
+++ app.js
@@ -31,14 +31,35 @@ const listCommands = ["pelit", "kalenteri", "tapahtumat"];
 const osallistujatCommands = ["osallistujat", "pelaajat"];
 const cancelCommands = ["peru", "pois"];
 
-/*
-For some reason node on server claims this does not exist
-So let's create it manually
-*/
-Date.prototype.getWeek = function () {
-  var dt = new Date(this.getFullYear(), 0, 1);
-  return Math.ceil(((this - dt) / 86400000 + dt.getDay() + 1) / 7);
-};
+// https://gist.github.com/catamphetamine/c0e2f21f1063b11a90f5790eadfcefa4
+function getWeek(date, dowOffset = 0) {
+  /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+  const newYear = new Date(date.getFullYear(), 0, 1);
+  let day = newYear.getDay() - dowOffset; //the day of week the year begins on
+  day = day >= 0 ? day : day + 7;
+  const daynum =
+    Math.floor(
+      (date.getTime() -
+        newYear.getTime() -
+        (date.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
+        86400000
+    ) + 1;
+  //if the year starts before the middle of a week
+  if (day < 4) {
+    const weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+    if (weeknum > 52) {
+      const nYear = new Date(date.getFullYear() + 1, 0, 1);
+      let nday = nYear.getDay() - dowOffset;
+      nday = nday >= 0 ? nday : nday + 7;
+      /*if the next year starts before the middle of
+        the week, it is week #1 of that year*/
+      return nday < 4 ? 1 : 53;
+    }
+    return weeknum;
+  } else {
+    return Math.floor((daynum + day - 1) / 7);
+  }
+}
 
 const parseDate = (date) => {
   let [dd, dm, dy] = date.split(".");
@@ -220,7 +241,7 @@ async function ilmoCommand(args, receivedMessage) {
     return;
   }
 
-  if (new Date().getWeek() !== new Date(ddate).getWeek()) {
+  if (getWeek(new Date(), 1) !== getWeek(new Date(ddate), 1)) {
     receivedMessage.channel.send(
       "Et voi ilmoittautua tulevien viikkojen turnauksiin."
     );
